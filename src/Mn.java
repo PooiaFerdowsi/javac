f7f8608c61554875c2d65206d0dfbbe9f6f154b7
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-24 18:18:20 +0430

    Update Mn.java

--- src/Mn.java
+++ src/Mn.java
@@ -1,7 +1,10 @@
 package src;
 
+import src.Tokenizer;
+
 public class Mn {
        public static void main(String s[]){
             System.out.print("asfl");
+            Tokenizer.go();
        }
 }
