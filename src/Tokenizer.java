4f5be4239fd3e5db0e0d7f6229d609f3458570f9
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-24 18:21:34 +0430

    Update Tokenizer.java

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -2,14 +2,7 @@ package src;
 
 
 class Tokenizer {
-     public ifTokenBeIn (String tok){
-           switch(tok){
-                 case "@":
-                     return "at tkn";
-                     break;
-           }
-     }
-     public state go(){
+     public static go(){
            System.out.print("\n go");
     }
 }
