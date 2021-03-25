0f5c9f70190c4a381387e52f44151260a58c8d7a
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-25 13:18:44 +0430

    Update Mn.java

--- Mn.java
+++ Mn.java
@@ -1,6 +1,13 @@
 public class Mn {
       public static void main (String s[]){
-            System.out.print("&$");
+            System.out.print("&$")
+            static int i;
+            String vars[];
+            String vals[];
             String cd = "$var;";
+            if(cd[0] == "$"){
+                 vars[i] = cd[1];
+                 ++i;
+            }
       }
 }
